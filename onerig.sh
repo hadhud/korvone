594e1f3fcf37f016efaa52c6ab962dce72145495
hadhud <94522719+hadhud@users.noreply.github.com>
2024-02-07 12:24:15 +0100

    Add files via upload

---
+++ onerig.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gnome-terminal -- sudo bash -c "/home/sm1/Downloads/xmrig-6.20.0/xmrig; exec bash"
---
+++ onessd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gnome-terminal -- sudo bash -c "cd /home/sm1/Downloads/nossd-2.2/linux/; ./smf.sh exec bash"
+
+
+
